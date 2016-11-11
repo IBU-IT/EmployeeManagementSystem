dc76be8b8dfad2e421aa6bbddd91d58793a5df5f
Demir Korac <demir-korac@hotmail.com>
2016-11-11 02:39:25 +0100

    Version2

--- EmployeeManagementSystem/src/ems/main.java
+++ EmployeeManagementSystem/src/ems/main.java
@@ -1,5 +1,9 @@
 package ems;
 
-public class main {
+import javax.swing.JOptionPane;
 
+public class main {
+	public static void main(String[]args){
+	JOptionPane.showMessageDialog(null,"Unesi broj 1");
+}
 }
