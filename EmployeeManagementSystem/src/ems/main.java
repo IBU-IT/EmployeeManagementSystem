b2abc763080e2f6964945219d5ba134c73f7c4dc
asmir <asmir@DESKTOP-0H03BEJ>
2016-11-11 02:51:08 +0100

    verzija4

--- EmployeeManagementSystem/src/ems/main.java
+++ EmployeeManagementSystem/src/ems/main.java
@@ -4,8 +4,8 @@ import javax.swing.JOptionPane;
 
 public class main {
 	public static void main(String[]args){
-	JOptionPane.showMessageDialog(null,"Unesi broj 1");
-	JOptionPane.showMessageDialog(null, "je l ti radi ovo?");
-	// asmir asmir asmir
-}
+		JOptionPane.showMessageDialog(null,"Unesi broj 1");
+		JOptionPane.showMessageDialog(null, "je l ti radi ovo?");
+		// asmir asmir asmir
+	}
 }
